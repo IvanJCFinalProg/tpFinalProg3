9982d4fbec904a802ba68dad6f75883b1a60477c
jcMarentette <33732409+jcMarentette@users.noreply.github.com>
2017-11-20 12:48:34 -0500

    lez go

--- TPSyntheseApp/src/cal/tpfinal/bean/Member.java
+++
@@ -1,5 +0,0 @@
-package cal.tpfinal.bean;
-
-public class Member {
-
-}
--- TPSyntheseApp/src/cal/tpfinal/util/IFile.java
+++
@@ -1,7 +0,0 @@
-package cal.tpfinal.util;
-
-public interface IFile {
-
-	public static String FILE1 ="C:\\appBasesDonnees/appRessources/appRessourcesData.properties"; 
-	public static String FILE2 ="C:\\appBasesDonnees/appRessources/appRessourcesFiles.properties"; 
-}
